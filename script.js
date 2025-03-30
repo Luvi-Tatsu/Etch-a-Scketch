b2feb49c02bb3e00a67cc00eb220ae5800c77a83
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-30 20:17:47 +0800

    Add Elements

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container");
+
+const button = document.createElement("button");
+button.classList.add("button");
+container.appendChild(button);
+
+const box = document.createElement("div");
+box.classList.add("box");
+container.appendChild(box);
+
