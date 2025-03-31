1d5210b32db58fc33726fdea3325a5dd8d408bca
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-31 13:08:22 +0800

    Create the BaseOutline

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const header = document.querySelector("#header");
 const container = document.querySelector("#container");
 
+const title = document.createElement("h1");
+    title.textContent="Etch A Sketch";
+   header.appendChild(title); 
 const button = document.createElement("button");
 button.classList.add("button");
-container.appendChild(button);
+button.textContent = "Create a Number of Grid";
+button.addEventListener('click', promptFN);
+function promptFN(e){
+    numBer = prompt("numbers"); 
+    console.log(numBer);
+}
+header.appendChild(button);
 
-const box = document.createElement("div");
-box.classList.add("box");
-container.appendChild(box);
+
+function column() {
+    for (let x = 0; x < 272; x++){
+        let box = document.createElement("div");
+        box.classList.add("box");
+            container.appendChild(box); 
+    }
+}
+    column() 
+
+    
 
