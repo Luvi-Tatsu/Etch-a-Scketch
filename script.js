83c742b921ac4d5a5d4ffe640d23d631accb5e5b
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-01 12:35:40 +0800

    add clear function and modify loop abit

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 const header = document.querySelector("#header");
 const container = document.querySelector("#container");
-
-function column() {
-    for (let x = 0; x < 272; x++){
-         let box = document.createElement("div");
-         box.classList.add("box");
-         container.appendChild(box); 
-    }
-}
-    column() 
-
 const title = document.createElement("h1");
       title.textContent="Etch A Sketch";
       header.appendChild(title); 
 const button = document.createElement("button");
-button.classList.add("button");
-button.textContent = "Create a Number of Grid";
-button.addEventListener('click', promptFN);
-function promptFN(e){
-    numBer = prompt("numbers"); 
-    console.log(numBer);
-    if (numBer > ''){
-        document.body.removeChild(container);
+      button.classList.add("button");
+      button.textContent = "Create a Number of Grid";
+      header.appendChild(button);
+
+const buttonClear = document.createElement("button");
+      buttonClear.classList.add("buttonClear");
+      buttonClear.textContent = "Clear";
+      header.appendChild(buttonClear);
+      
+       
+    for (let x = 0; x < 272; x++){
+        createBox();
     }
-}
-header.appendChild(button);
 
-
-
-    
-
+    function createBox(){
+        let box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);         
+        box.addEventListener('mouseover', function(){
+        box.style.backgroundColor = 'black';});
+        buttonClear.addEventListener('click',function(){
+        box.style.backgroundColor = 'white';});
+    }
