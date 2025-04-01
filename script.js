31bae50fb26266f888d2bb4bcfe5d26da0916dbd
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-01 10:44:36 +0800

    delete existing grid upon prompt

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const header = document.querySelector("#header");
 const container = document.querySelector("#container");
 
+function column() {
+    for (let x = 0; x < 272; x++){
+         let box = document.createElement("div");
+         box.classList.add("box");
+         container.appendChild(box); 
+    }
+}
+    column() 
+
 const title = document.createElement("h1");
-    title.textContent="Etch A Sketch";
-   header.appendChild(title); 
+      title.textContent="Etch A Sketch";
+      header.appendChild(title); 
 const button = document.createElement("button");
 button.classList.add("button");
 button.textContent = "Create a Number of Grid";
@@ -11,18 +20,13 @@ button.addEventListener('click', promptFN);
 function promptFN(e){
     numBer = prompt("numbers"); 
     console.log(numBer);
+    if (numBer > ''){
+        document.body.removeChild(container);
+    }
 }
 header.appendChild(button);
 
 
-function column() {
-    for (let x = 0; x < 272; x++){
-        let box = document.createElement("div");
-        box.classList.add("box");
-            container.appendChild(box); 
-    }
-}
-    column() 
 
     
 
