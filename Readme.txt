edfc232e8c6df3e520cef35efdb5a5329329f925
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-02 11:07:31 +0800

    Added button functions

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,4 @@
 Etch a Sketch Project (Mar. 30, 2025)
+
+google's ai helped in finding the right code on specifications of prompt values.
+google's ai also help me somewhat realize what if return statements is capable of;
--- script.js
+++ script.js
@@ -1,29 +1,62 @@
 const header = document.querySelector("#header");
 const container = document.querySelector("#container");
+//Title
 const title = document.createElement("h1");
       title.textContent="Etch A Sketch";
       header.appendChild(title); 
+//Create Grid button
 const button = document.createElement("button");
       button.classList.add("button");
-      button.textContent = "Create a Number of Grid";
+      button.textContent = "Change Grid";
       header.appendChild(button);
-
+//Clear button
 const buttonClear = document.createElement("button");
       buttonClear.classList.add("buttonClear");
       buttonClear.textContent = "Clear";
       header.appendChild(buttonClear);
-      
-       
-    for (let x = 0; x < 272; x++){
+
+    for (let x = 0; x < 256; x++){
         createBox();
     }
 
     function createBox(){
+        let init = 17;
+        let sizer = 100;
         let box = document.createElement("div");
+        let flexBasis = sizer / init;
         box.classList.add("box");
-        container.appendChild(box);         
+        container.appendChild(box); 
+        box.style.flexBasis = `${flexBasis}% `;        
         box.addEventListener('mouseover', function(){
         box.style.backgroundColor = 'black';});
         buttonClear.addEventListener('click',function(){
         box.style.backgroundColor = 'white';});
     }
+
+    button.addEventListener('click', function(){
+      let newSize = prompt("");
+      newSize = parseInt(newSize);
+      if (newSize === null || newSize === ''){
+            return;
+      }
+      if (isNaN(newSize) || newSize<1 || newSize > 100){
+            alert("We can only put numbers from 1 to 100");
+            return;
+      }
+       else {container.innerHTML = '';}
+       if(container.innerHTML === ''){
+            for (let x = 0; x < newSize * newSize + newSize; x++){
+                  let size = newSize +1;
+                  let initial = 100;
+                  let box = document.createElement("div");
+                  let boxSize = initial/size;
+                  box.classList.add("box");
+                  box.style.flexBasis = `${boxSize}%`;
+                  container.appendChild(box);         
+                  box.addEventListener('mouseover', function(){
+                  box.style.backgroundColor = 'black';});
+                  buttonClear.addEventListener('click',function(){
+                  box.style.backgroundColor = 'white';});
+    }
+              }
+       })
